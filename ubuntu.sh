0b3672030978379e968358634c0dc9cb7eb704dc
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-09-21 22:59:46 -0300

    update ubuntu and start installation script

---
+++ ubuntu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+ 
+echo "Joel's Ubuntu Setup Script"
+
+# Copy bash rc to home foler
+cp .bashrc ~/
+
+# Update
+apt-get update
+
+# Tree
+apt-get install tree
+
+# sl
+apt-get install sl
+
+#htop
+apt-get install htop
+
+# Ruby
+apt-get install ruby
+
+# Node.js
+add-apt-repository ppa:chris-lea/node.js
+sudo apt-get install nodejs
+
+# Bower
+npm install -g bower
+
+# Grunt
+npm install -g grunt-cli
+
+# Gulp
+npm install -g gulp
+
+# Yeoman
+npm install -g yo
